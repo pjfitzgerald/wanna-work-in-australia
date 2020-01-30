2fe3396a20cb8a8e9f4b48bced47b913e04f7945
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-01-30 17:06:42 +1100

    job.create method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,12 +6,28 @@ class JobsController < ApplicationController
   end
 
   def create
-    @
+    @job = Job.new(job_params)   #double check syntax for retrieving params
+
+    #  check for empty fields, refactor to loop over attributes instead of if-statements
+    if params[:job][:title].empty?
+      flash[:notice] = "Woops! Please enter a title!"
+    elsif params[:job][:description].empty?
+      flash[:notice] = "Woops! Please enter a description!"
+    else
+      if @job.save
+        flash[:notice] = "Job submitted"
+      else
+        flash[:notice] = "Something went wrong!"
+      end
+    end
+    redirect_back(fallback_location: dashboard_path)
   end
 
 
   private
 
   def job_params
-    params.require(:job).permit(:)
+    params.require(:job).permit(:title, :description)
+  end
+
 end
