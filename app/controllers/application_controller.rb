a7ed416df8aee38085ab231ffff91e6f73475d58
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-02-04 00:19:12 +1100

    meta config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def index
-    @user = User.find(current_user.id)
-    @applications = Application.where(traveller: @user)
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
-  def new
-    @application = Application.new
-  end
-
-  def create
-    @application = Application.create!(traveller: current_user)
-  end
-
-  private
-
-  def application_params
-    params.require(:restaurant).permit(:user_id)
-  end
 end
 
--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,23 @@
+
+# this is not 'application_controller.rb !! '
+
 class ApplicationsController < ApplicationController
+  def index
+    @user = User.find(current_user.id)
+    @applications = Application.where(traveller: @user)
+  end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.create!(traveller: current_user)
+  end
+
+  private
+
+  def application_params
+    params.require(:restaurant).permit(:user_id)
+  end
 end
