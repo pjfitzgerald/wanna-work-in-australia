cec8e321766ff6779ca65c016fabd2cde680407d
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-02-12 20:31:43 +1100

    places and geocoding started

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,5 @@
 function initMap(lat, lng) {
-  var myCoords = new google.maps.LatLng(lat, lng);
+  var myCoords = new google.maps.LatLng(lat, lng);    // map initialisation location
 
   var mapOptions = {
     center: myCoords,
@@ -9,7 +9,27 @@ function initMap(lat, lng) {
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
   
   var marker = new google.maps.Marker({
-    position: (myCoords),   // Stones location
-    map: map
+    position: (myCoords),
+    map: map,
+    animation: google.maps.Animation.DROP
   });
-}
+
+
+  // geocoding to address on-click
+
+  var geocoder = new google.maps.Geocoder();
+
+  google.maps.event.addListener(map, 'click', function (event) {
+    geocoder.geocode({
+      'latLng': event.latLng
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          alert(results[0].formatted_address);
+        }
+      }
+    })
+    }
+  );
+
+};
