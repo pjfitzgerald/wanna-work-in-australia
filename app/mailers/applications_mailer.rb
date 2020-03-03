b5f2b6a76639f1486c31d2af8c22313bbc31e881
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-03-03 22:42:38 +1100

    small adjustments

--- app/mailers/applications_mailer.rb
+++ app/mailers/applications_mailer.rb
@@ -6,7 +6,7 @@ class ApplicationsMailer < ApplicationMailer
     @venue = @job.venue
     @applications_path = "/users/#{@user.id}/applications"
     # make sure all venues have an email on file, or adjust this to account for lack of an address
-    mail(to: @venue.email, subject: "#{@user.first_name} #{@user.last_name} has submitted an application to work for your venue")
+    # mail(to: @venue.email, subject: "#{@user.first_name} #{@user.last_name} has submitted an application to work for your venue")
     mail(to: @user.email, subject: "Your application has been submitted to #{@venue.name}")
   end
 
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,12 +1,15 @@
 class Application < ApplicationRecord
+  include ActiveModel::Validations
+  
   belongs_to :traveller
   belongs_to :job
   has_many :reviews
 
-  has_one_attached :resume # test attachment to each application individually
-
   validates :length_of_stay, presence: true
   validates :country_of_origin, presence: true
+
+  has_one_attached :resume
+  # validates :resume, attached: true # calls custom validator in app/validators/attached_validator.rb --AS doesn't currently support validations
 
   def country_name
     country = ISO3166::Country[country_code]
