cd73167a9f22135ca5b9076d176a1be306e71127
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-03-17 22:03:13 +1100

    state cards in regions/index

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,8 +1,20 @@
+require_relative '../../config/initializers/wwia_constants.rb'
+
 class RegionsController < ApplicationController
   skip_before_action :authenticate_user!
   
   def index
     @regions = Region.all
+    @states = [
+      ["Victoria", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643782/wwia/region%20banners/the_grampians_ojkaom.jpg"],
+      ["New South Wales", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643771/wwia/region%20banners/the_murray_ffm5r7.jpg"],
+      ["Queensland", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643717/wwia/region%20banners/brisbane_wemqit.jpg"],
+      ["South Australia", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643705/wwia/region%20banners/barossa_valley_x0vejh.jpg"],
+      ["Northern Territory", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643706/wwia/region%20banners/darwin_dirruw.jpg"],
+      ["Western Australia", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643734/wwia/region%20banners/margaret_river_southwest_ljcref.jpg"],
+      ["Tasmania", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643741/wwia/region%20banners/hobart_and_the_south_kqlwgg.jpg"],
+      ["Australian Capital Territory", "https://res.cloudinary.com/dkowfxwpp/image/upload/v1580643701/wwia/region%20banners/canberra_and_surrounds_oced83.jpg"]
+  ]
   end
 
   def vic
@@ -50,7 +62,6 @@ class RegionsController < ApplicationController
   def show
     @region = Region.find(params[:id])
     @venues = Venue.where(region: @region)
-    # @jobs = Job.where(status: "Open", venue: @venues. )
   end
 
 
