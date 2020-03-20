06f57c384fd97e3007032ff04d504acbe6438974
patrickfitzgerald <patrick.fitzgerald29@gmail.com>
2020-03-20 20:23:08 +1100

    rectify country_of_origin - country_name retrieval

--- app/models/application.rb
+++ app/models/application.rb
@@ -12,7 +12,7 @@ class Application < ApplicationRecord
   # validates :resume, attached: true # calls custom validator in app/validators/attached_validator.rb --AS doesn't currently support validations
 
   def country_name
-    country = ISO3166::Country[country_code]
+    country = ISO3166::Country[country_of_origin]
     country.translations[I18n.locale.to_s] || country.name
   end
 end
